6b4d9eccb9018b61fa7d44054ce301fd84a0e134
Matt Henry <matt.henry@gsa.gov>
2019-09-27 16:10:19 -0400

    move focus to modal when visible

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,9 +1,11 @@
 (function(){
     var teamLinks = Array.from(document.getElementsByClassName('link__team-member'));
+    var originalFocusedEl;
 
     teamLinks.map(function(link) {
         link.addEventListener('click', function(e){
         e.preventDefault();
+        originalFocusedEl = e.target;
         loadTeamMember(e.target);
         });
     });
@@ -22,7 +24,7 @@
         $.get(teamMemberURL, function(data){
         var page = $('#main', data)[0];
         $('#bio', $shell).html(page);
-        $shell.show();
+        $shell.show().focus();
         $('#modal-overlay').show();
         });
     };
@@ -30,5 +32,6 @@
     function dismissModal(){
         $('#modal-overlay').hide();
         $('#modal-inner').hide();
+        originalFocusedEl.focus();
     }
 })()
